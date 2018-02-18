d1314292537228dc7a92f75631689ce3fcb7bb78
ksenksenon <ksekataeva@yandex.ru>
2018-02-18 13:58:21 +0500

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -12,8 +12,10 @@ class Post(models.Model):
             blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+        self.published_date = timezone.now()        
+        self.save()        
+	
+        
 
     def __str__(self):
         return self.title
